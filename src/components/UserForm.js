e611fa9e2a6445e01b2a200f1e0083f8564a3edf
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-14 16:38:32 -0500

    import yup

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,7 +1,7 @@
 /// external modules ///
 import React from 'react';
 import { withFormik , Form , Field } from 'formik';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 // import axios from 'axios';
 
 /***************************************
