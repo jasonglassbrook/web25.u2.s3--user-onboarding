2546da8b72b02d74845e1477f0d1025fbb6c4e08
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-14 15:42:23 -0500

    import formik, yup, axios -- //ed out

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,8 @@
 /// external modules ///
 import React from 'react';
+// import { withFormik , Form , Field } from 'formik';
+// import * as Yup from 'yup';
+// import axios from 'axios';
 
 /***************************************
   STATES
